040306c13be2b972168c94d2bc8272b1807de6b6
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-06-23 07:52:15 +1000

    Added build-clean.ts

---
+++ scripts/build-clean.ts
@@ -0,0 +1,7 @@
+/** Brute force way to make sure all compiled ts files are removed. `tsc --build --clean` didn't always work as intended. */
+
+import { walkdirSync } from 'more-node-fs';
+import { unlinkSync } from 'fs';
+import { join } from 'path';
+for (const { path, stats } of walkdirSync(join(__dirname, '../'), { ignore: /node_modules/i }))
+  if (stats.isFile() && /(\.d\.ts|\.js)$/.test(path)) unlinkSync(path);
