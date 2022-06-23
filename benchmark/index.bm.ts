4741dcf1bd8f90608961108928e474ff37442f13
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-06-23 10:19:11 +1000

    Cleaned up bm

--- benchmark/index.bm.ts
+++ benchmark/index.bm.ts
@@ -1,13 +1,6 @@
 import { setupSuite } from './bm-util';
 
-// function getRandInt (min, max) {
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
 const suite = setupSuite(__filename.split(/[\\/]/).pop());
-// import { range, iterate, iterate2, ExtendedIterator, IteratorWithOperators } from '../../utils/iterutil';
 import ExtendedIterator from '../src/ExtendedIterator';
 import { IteratorWithOperators } from 'iterare/lib/iterate';
 import { iterplus } from 'iterplus';
@@ -17,39 +10,31 @@ function* nums(size: number) {
 }
 
 for (const size of [1e4 /*, 1e5, 1e6 */]) {
-  // suite.add(`for of loop ${1e4}`, () => {
-  //   const arr: string[] = [];
-  //   for (const x of nums(size))
-  //     arr.push((x * x).toString());
-  // });
-  // suite.add(`iteragain ${size}`, () => {
-  //   new ExtendedIterator(nums(size))
-  //     .map(x => x * x)
-  //     // .filter(x => x % 2 !== 0)
-  //     .map(x => x.toString())
-  //     .toArray();
-  // });
-  // suite.add(`iterare ${size}`, () => {
-  //   new IteratorWithOperators(nums(size))
-  //     .map(x => x * x)
-  //     // .filter(x => x % 2 !== 0)
-  //     .map(x => x.toString())
-  //     .toArray();
-  // });
-  // suite.add(`iterplus ${size}`, () => {
-  //   iterplus(nums(size))
-  //     .map(x => x * x)
-  //     // .filter(x => x % 2 !== 0)
-  //     .map(x => x.toString())
-  //     .collect();
-  // });
-  suite.add('for of', () => {
+  suite.add(`for of loop ${1e4}`, () => {
     const arr: string[] = [];
-    for (const x of new ExtendedIterator(nums(size)).map(x => x * x).map(x => x.toString())) arr.push(x);
+    for (const x of nums(size))
+      arr.push((x * x).toString());
   });
-  suite.add('for of 2', () => {
-    const arr: string[] = [];
-    for (const x of new ExtendedIterator(nums(size)).map(x => x * x).map(x => x.toString())) arr.push(x);
+  suite.add(`iteragain ${size}`, () => {
+    new ExtendedIterator(nums(size))
+      .map(x => x * x)
+      // .filter(x => x % 2 !== 0)
+      .map(x => x.toString())
+      .toArray();
+  });
+  suite.add(`iterare ${size}`, () => {
+    new IteratorWithOperators(nums(size))
+      .map(x => x * x)
+      // .filter(x => x % 2 !== 0)
+      .map(x => x.toString())
+      .toArray();
+  });
+  suite.add(`iterplus ${size}`, () => {
+    iterplus(nums(size))
+      .map(x => x * x)
+      // .filter(x => x % 2 !== 0)
+      .map(x => x.toString())
+      .collect();
   });
 }
 
