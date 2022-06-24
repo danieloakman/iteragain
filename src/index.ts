6e045bb51260dcdd6c3c7350eb2b720cb651f7dc
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-06-24 12:25:21 +1000

    Changed these to use the default exports

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { concat } from './concat';
+import concat from './concat';
 import enumerate from './enumerate';
 import ExtendedIterator from './ExtendedIterator';
-import { flatten } from './flatten';
+import flatten from './flatten';
 import isIterable from './isIterable';
 import isIterator from './isIterator';
 import iter from './iter';
 import range from './range';
 import toIterator from './toIterator';
-import { zip } from './zip';
-import { zipLongest } from './zipLongest';
+import zip from './zip';
+import zipLongest from './zipLongest';
 
 export {
   concat,
