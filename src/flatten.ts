e4e723a81db3e782886b4b0101a66dcd44ff0c6c
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-06-27 22:46:45 +1000

    `flatten` now uses FlattenIterator

--- src/flatten.ts
+++ src/flatten.ts
@@ -1,22 +1,7 @@
 import ExtendedIterator from './ExtendedIterator';
 import { IteratorOrIterable, FlattenDeep, FlattenDepth1, FlattenDepth2, FlattenDepth3, FlattenDepth4, FlattenDepth5 } from './types';
-import isIterable from './isIterable';
-import isIterator from './isIterator';
+import FlattenIterator from './internal/FlattenIterator';
 import toIterator from './toIterator';
-
-function* flattenGen<T>(arg: IteratorOrIterable<T>, depth: number) {
-  const iterator = toIterator(arg);
-  let next: IteratorResult<any>;
-  if (depth === 0) {
-    while (!(next = iterator.next()).done) yield next.value;
-    return;
-  }
-  while (!(next = iterator.next()).done) {
-    if (isIterable(next.value) || isIterator(next.value)) yield* flattenGen(next.value, depth - 1);
-    else yield next.value;
-  }
-}
-
 
 /**
  * Flattens an iterator or iterable.
@@ -31,7 +16,7 @@ export function flatten<T>(arg: IteratorOrIterable<T>, depth: 5): ExtendedIterat
 export function flatten<T>(arg: IteratorOrIterable<T>): ExtendedIterator<FlattenDeep<T>>;
 export function flatten<T>(arg: IteratorOrIterable<T>, depth: number): ExtendedIterator<any>;
 export function flatten<T>(arg: IteratorOrIterable<T>, depth = Infinity) {
-  return new ExtendedIterator(flattenGen(arg, depth));
+  return new ExtendedIterator(new FlattenIterator(toIterator(arg), depth));
 }
 
 export default flatten;
---
+++ src/internal/FlattenIterator.ts
@@ -0,0 +1,28 @@
+import isIterable from '../isIterable';
+import isIterator from '../isIterator';
+import toIterator from '../toIterator';
+
+/** Flattens an iterator `depth` number of levels. */
+export class FlattenIterator implements Iterator<any> {
+  protected inner: Iterator<any> = null;
+  constructor(protected readonly iterator: Iterator<any>, protected depth: number) {}
+
+  next() {
+    if (this.depth < 1) return this.iterator.next();
+    let next: IteratorResult<any>;
+    if (this.inner) {
+      next = this.inner.next();
+      if (next.done) {
+        this.inner = null;
+        return this.next();
+      }
+    } else next = this.iterator.next();
+    if (isIterable(next.value) || isIterator(next.value)) {
+      this.inner = new FlattenIterator(toIterator(next.value), this.depth - 1);
+      return this.inner.next();
+    }
+    return next;
+  }
+}
+
+export default FlattenIterator;
