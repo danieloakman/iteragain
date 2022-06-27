24bee80ced8ea22641a172088f545e9a2398878d
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-06-27 17:59:27 +1000

    Some changes after testing with rxjs as well

--- benchmark/index.bm.ts
+++ benchmark/index.bm.ts
@@ -3,40 +3,63 @@ import { setupSuite } from './bm-util';
 const suite = setupSuite(__filename.split(/[\\/]/).pop());
 import ExtendedIterator from '../src/ExtendedIterator';
 import { IteratorWithOperators } from 'iterare/lib/iterate';
-import { iterplus } from 'iterplus';
+import { from } from 'rxjs';
+import { map, filter/* , skip */ } from 'rxjs/operators';
 
 function* nums(size: number) {
   for (let i = 0; i < size; i++) yield i;
 }
 
-for (const size of [1e4 /*, 1e5, 1e6 */]) {
-  suite.add(`for of loop ${1e4}`, () => {
-    const arr: string[] = [];
-    for (const x of nums(size))
-      arr.push((x * x).toString());
-  });
-  suite.add(`iteragain ${size}`, () => {
-    new ExtendedIterator(nums(size))
-      .map(x => x * x)
-      // .filter(x => x % 2 !== 0)
-      .map(x => x.toString())
-      .toArray();
-  });
-  suite.add(`iterare ${size}`, () => {
-    new IteratorWithOperators(nums(size))
-      .map(x => x * x)
-      // .filter(x => x % 2 !== 0)
-      .map(x => x.toString())
-      .toArray();
-  });
-  suite.add(`iterplus ${size}`, () => {
-    iterplus(nums(size))
-      .map(x => x * x)
-      // .filter(x => x % 2 !== 0)
-      .map(x => x.toString())
-      .collect();
-  });
-}
+const SIZE = 1e4;
+
+// for (const { IterClass, name } of [
+//   { IterClass: ExtendedIterator, name: 'iteragain' },
+//   { IterClass: IteratorWithOperators, name: 'iterare' },
+//   // { IterClass: IterPlus, name: 'iterplus' },
+// ]) {
+//   suite.add(name, () => {
+//     new IterClass(nums(SIZE))
+//       // .slice(0, SIZE / 2)
+//       .map(n => n * n)
+//       .filter(n => n % 2 !== 0)
+//       // .filter(n => n > 100)
+//       // .filter(n => n < 200)
+//       .map(n => n.toString())
+//       .toArray();
+//   });
+// }
+
+// suite.add('for of loop', () => {
+//   const arr: string[] = [];
+//   for (const x of nums(SIZE))
+//     arr.push((x * x).toString());
+// });
+suite.add('iteragain', () => {
+  new ExtendedIterator(nums(SIZE))
+    .map(x => x * x)
+    .filter(x => x % 2 !== 0)
+    .map(x => x.toString())
+    .toArray();
+});
+suite.add('iterare', () => {
+  new IteratorWithOperators(nums(SIZE))
+    .map(x => x * x)
+    .filter(x => x % 2 !== 0)
+    .map(x => x.toString())
+    .toArray();
+});
+suite.add('rxjs', () => {
+  from(nums(SIZE))
+    .pipe(
+      // skip(SIZE / 2),
+      map(n => n * n),
+      filter(n => n % 2 !== 0),
+      // filter(n => n > 100),
+      // filter(n => n < 200),
+      map(n => n.toString()),
+    )
+    .subscribe(() => {});
+});
 
 suite.run({
   async: true,
