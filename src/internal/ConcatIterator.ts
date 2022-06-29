7a1ded55482184f4033a1afc50e33b8f7aa83264
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-06-29 23:35:28 +1000

    Refactored ConcatIterator

--- src/internal/ConcatIterator.ts
+++ src/internal/ConcatIterator.ts
@@ -1,16 +1,13 @@
 /** An iterator that concatenates other iterators together, in the order they are in the `iterators` arg. */
 export class ConcatIterator implements Iterator<any> {
-  protected nextIterator = this.iterators.shift();
   constructor(protected iterators: Iterator<any>[]) {}
 
   next(): IteratorResult<any> {
-    const next = this.nextIterator.next();
+    if (!this.iterators.length) return { done: true, value: undefined };
+    const next = this.iterators[0].next();
     if (!next.done) return next;
-    if (this.iterators.length) {
-      this.nextIterator = this.iterators.shift();
-      return this.next();
-    }
-    return { done: true, value: undefined };
+    this.iterators.shift();
+    return this.next();
   }
 }
 
