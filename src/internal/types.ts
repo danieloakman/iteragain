dc549b9c6d829ea305e9564fec211a7604e41d59
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-07-06 20:55:31 +1000

    Added `Predicate` and `Iteratee` types

--- src/internal/types.ts
+++ src/internal/types.ts
@@ -15,3 +15,9 @@ export type FlattenDepth5<T> = T extends IteratorOrIterable<infer V> ? FlattenDe
 
 type _TupleOf<T, N extends number, R extends unknown[]> = R['length'] extends N ? R : _TupleOf<T, N, [T, ...R]>;
 export type Tuple<T, N extends number> = N extends N ? (number extends N ? T[] : _TupleOf<T, N, []>) : never;
+
+/** A function that returns a truthy value given an input value of type `T`. */
+export type Predicate<T> = (value: T) => any;
+
+/** A function that does something with value of type `T` and transforms it into type `R`. */
+export type Iteratee<T, R> = (value: T) => R;
