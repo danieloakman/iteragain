cf666f2c4569d8337512383f7ddcc9ca2671a66d
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-07-21 14:32:02 +1000

    Handle README.md in publish-es

--- scripts/publish-es.ts
+++ scripts/publish-es.ts
@@ -7,6 +7,8 @@ try {
 
   const packageJSONPath = join(__dirname, '../package.json');
   const packageLockJSONPath = join(__dirname, '../package-lock.json');
+  const readmePath = join(__dirname, '../README.md');
+  const readmeESPath = join(__dirname, '../README.es.md');
   const packageJSON = JSON.parse(readFileSync(packageJSONPath, 'utf8'));
   const packageLockJSON = JSON.parse(readFileSync(packageLockJSONPath, 'utf8'));
   packageJSON.name = packageLockJSON.name = 'iteragain-es';
@@ -14,9 +16,10 @@ try {
   packageJSON.scripts.prepare = packageJSON.scripts.prepare.replace('npm run build', 'npm run build:es');
   writeFileSync(packageJSONPath, JSON.stringify(packageJSON, null, 2));
   writeFileSync(packageLockJSONPath, JSON.stringify(packageLockJSON, null, 2));
+  writeFileSync(readmePath, readFileSync(readmeESPath, 'utf8'));
 
   execSync('npm publish', { stdio: 'inherit' });
 } finally {
   // Cleanup
-  execSync('git checkout package.json package-lock.json && npm run build:clean', { stdio: 'inherit' });
+  execSync('git checkout package.json package-lock.json README.md && npm run build:clean', { stdio: 'inherit' });
 }
