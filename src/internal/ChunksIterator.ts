a66aabce8b13b7a656ce382335e517998da29ffa
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-08-04 06:42:31 +1000

    Fixed an infinite iteration bug with ChunksIterator if the input iterator could continue after having emitted a done IteratorResult

--- src/internal/ChunksIterator.ts
+++ src/internal/ChunksIterator.ts
@@ -1,26 +1,34 @@
 /** An iterator that yields non-overlapping values in chunks (tuples) of a certain `size`. */
 export class ChunksIterator<T> implements IterableIterator<T[]> {
+  protected done = false;
+  protected chunk: T[] = [];
+
   constructor(protected iterator: Iterator<T>, protected length: number, protected fill?: T | null) {}
 
   [Symbol.iterator](): IterableIterator<T[]> {
     return this;
   }
 
+  return(value?: any): IteratorResult<T[], any> {
+    return value;
+  }
+
   next(): IteratorResult<T[]> {
-    let chunk: T[] = [];
+    if (this.done) return { done: true, value: undefined };
     for (let i = 0; i < this.length; i++) {
       const next = this.iterator.next();
       if (next.done) {
-        if (chunk.length) {
+        this.done = true;
+        if (this.chunk.length) {
           if (this.fill !== undefined)
-            chunk = chunk.concat(Array.from({ length: this.length - chunk.length }, _ => this.fill));
+            this.chunk = this.chunk.concat(Array.from({ length: this.length - this.chunk.length }, _ => this.fill));
           break;
         }
         return { done: true, value: undefined };
       }
-      chunk.push(next.value);
+      this.chunk.push(next.value);
     }
-    return { done: false, value: chunk };
+    return { done: false, value: this.chunk.splice(0, this.length) };
   }
 }
 
