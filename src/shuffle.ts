bf85f60199eb208740ba934ef82a80330eea4a59
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-10-18 13:59:20 +1100

    Added shuffle JSDoc

--- src/shuffle.ts
+++ src/shuffle.ts
@@ -1,6 +1,7 @@
 import { IteratorOrIterable } from './internal/types';
 import toArray from './toArray';
 
+/** Collects all values from the input iterator, then shuffles the order of it's values. */
 export function shuffle<T>(arg: IteratorOrIterable<T>): IterableIterator<T> {
   const values = toArray(arg);
   for (let i = values.length - 1; i > 0; i--) {
