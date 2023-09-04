981d08628b1f5372d159bc9f10c01c1720cee080
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-09-04 17:01:11 +1000

    `reverse` now uses toIterableIterator

--- src/reverse.ts
+++ src/reverse.ts
@@ -1,8 +1,8 @@
 import { IteratorOrIterable } from './types';
 import toIterator from './toIterator';
+import toIterableIterator from './toIterableIterator';
 
 /**
- * @lazy
  * Reverses the input iterator's order. Note that in order to reverse, it will attempt to iterate fully once, which
  * could cause significant memory usage. So because of this, only use on finite iterators.
  */
@@ -11,11 +11,7 @@ export function reverse<T>(arg: IteratorOrIterable<T>): IterableIterator<T> {
   const it = toIterator(arg);
   const result: T[] = [];
   while (!(next = it.next()).done) result.unshift(next.value);
-  return Object.assign(toIterator(result), {
-    [Symbol.iterator]() {
-      return this;
-    },
-  }) as IterableIterator<T>;
+  return toIterableIterator(result);
 }
 
 export default reverse;
