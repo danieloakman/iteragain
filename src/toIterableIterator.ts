19e0bcc133360fa239fbacb3bc968b6c0c987124
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-09-04 16:59:49 +1000

    Added toIterableIterator

---
+++ src/toIterableIterator.ts
@@ -0,0 +1,14 @@
+import toIterator from './toIterator';
+import { IteratorOrIterable } from './types';
+
+/** Converts any Iterator or Iterable into a IterableIterator. */
+export function toIterableIterator<T>(arg: IteratorOrIterable<T>): IterableIterator<T> {
+  const iterator = toIterator(arg);
+  return Object.assign(iterator, {
+    [Symbol.iterator]() {
+      return this;
+    },
+  }) as IterableIterator<T>;
+}
+
+export default toIterableIterator;
