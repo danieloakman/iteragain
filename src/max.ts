0a6a186a26555fff955a14a05ff6f7c29bc07470
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-09-07 09:14:54 +1000

    Fixed max type bugs

--- src/max.ts
+++ src/max.ts
@@ -1,9 +1,11 @@
-import type { Iteratee, IteratorOrIterable } from './types';
+import type { IterSource, Iteratee, IteratorOrIterable } from './types';
 import toIterator from './toIterator';
 
 /** Returns the maximum value from the input iterator. */
-export function max<T>(iteratee?: Iteratee<T, number>): (arg: IteratorOrIterable<T>) => T;
-export function max<T>(arg: IteratorOrIterable<T>, iteratee?: Iteratee<T, number>): T;
+export function max<T extends IteratorOrIterable<unknown>>(
+  iteratee?: Iteratee<IterSource<T>, number>,
+): (arg: T) => IterSource<T>;
+export function max<T extends IteratorOrIterable<unknown>>(arg: T, iteratee?: Iteratee<IterSource<T>, number>): IterSource<T>;
 export function max(...args: any[]): unknown {
   if (!args.length || typeof args[0] === 'function') return (it: IteratorOrIterable<unknown>) => max(it, args[0]);
   const it = toIterator(args[0]);
