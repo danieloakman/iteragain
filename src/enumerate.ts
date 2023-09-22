474cceefd4bae8a280a70f1666c5dbd142034e6f
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-09-22 13:53:08 +1000

    Removed unnecessary overload

--- src/enumerate.ts
+++ src/enumerate.ts
@@ -3,8 +3,7 @@ import toIterator from './toIterator';
 import MapIterator from './internal/MapIterator';
 
 /** Attaches the index at each value of `arg`. */
-export function enumerate<T>(arg: IteratorOrIterable<T>): IterableIterator<[number, T]>;
-export function enumerate(arg: any): IterableIterator<[number, any]> {
+export function enumerate<T>(arg: IteratorOrIterable<T>): IterableIterator<[number, T]> {
   return new MapIterator(toIterator(arg), ((count = 0) => v => [count++, v])()); // prettier-ignore
 }
 
