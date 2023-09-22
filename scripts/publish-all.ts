956540e4279024fa06078f259afcc6c2d5c3b019
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-09-22 15:12:14 +1000

    publish-all now uses sh instead of execSync which is blocking

--- scripts/publish-all.ts
+++ scripts/publish-all.ts
@@ -1,38 +1,52 @@
-import { execSync } from 'child_process';
-import { readFileSync, writeFileSync } from 'fs';
+import { readFile, readdir, writeFile } from 'fs/promises';
 import { join } from 'path';
+import { sh, ok } from 'js-utils';
 
-function readJSONFile(path: string) {
-  return JSON.parse(readFileSync(path, 'utf8'));
+async function readJSONFile(path: string) {
+  return JSON.parse(await readFile(path, 'utf8'));
+}
+
+async function assertDirContainsJSFiles(dir: string) {
+  const files = await readdir(dir);
+  if (!files.some(p => p.endsWith('.js')))
+    throw new Error(`No compiled JS files found in "${dir}" folder. Build failed.`);
 }
 
 try {
+  const distDir = join(__dirname, '../');
+
   // Test and lint:
-  execSync('pnpm run check && pnpm run coverage && pnpm run lint', { stdio: 'inherit' });
+  ok(await sh('pnpm run check && pnpm run coverage && pnpm run lint'));
 
   // Clean, Build and Publish for common JS version ("iteragain"):
-  execSync('pnpm run build:clean && pnpm run build && pnpm publish', { stdio: 'inherit' });
+  ok(await sh('pnpm run build:clean && pnpm run build && pnpm publish'));
+
+  assertDirContainsJSFiles(distDir);
+
+  ok(await sh('pnpm publish'));
 
   // Clean, Build for ES modules:
-  execSync('pnpm run build:clean && pnpm run build:es', { stdio: 'inherit' });
+  ok(await sh('pnpm run build:clean && pnpm run build:es'));
+
+  assertDirContainsJSFiles(distDir);
 
   // Modify package.json and package-lock.json for ES modules version ("iteragain-es") publish:
   const packageJSONPath = join(__dirname, '../package.json');
   const packageLockJSONPath = join(__dirname, '../package-lock.json');
-  const packageJSON = readJSONFile(packageJSONPath);
-  const packageLockJSON = readJSONFile(packageLockJSONPath);
+  const packageJSON = await readJSONFile(packageJSONPath);
+  const packageLockJSON = await readJSONFile(packageLockJSONPath);
   const readmePath = join(__dirname, '../README.md');
   const readmeESPath = join(__dirname, '../README.es.md');
   packageJSON.name = packageLockJSON.name = 'iteragain-es';
   packageJSON.type = 'module';
   packageJSON.homepage += '-es';
   packageJSON.sideEffects = false;
-  writeFileSync(packageJSONPath, JSON.stringify(packageJSON, null, 2));
-  writeFileSync(packageLockJSONPath, JSON.stringify(packageLockJSON, null, 2));
-  writeFileSync(readmePath, readFileSync(readmeESPath, 'utf8'));
+  await writeFile(packageJSONPath, JSON.stringify(packageJSON, null, 2));
+  await writeFile(packageLockJSONPath, JSON.stringify(packageLockJSON, null, 2));
+  await writeFile(readmePath, await readFile(readmeESPath, 'utf8'));
 
-  execSync('pnpm publish --no-git-checks', { stdio: 'inherit' });
+  ok(await sh('pnpm publish --no-git-checks'));
 } finally {
   // Cleanup
-  execSync('git checkout package.json package-lock.json README.md && pnpm run build:clean', { stdio: 'inherit' });
+  ok(await sh('git checkout package.json package-lock.json README.md && pnpm run build:clean'));
 }
