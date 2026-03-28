5069ec8bb05b40df9c963dc06b3e48c5af4c4a8e
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2026-03-29 09:51:19 +1100

    Add the concurrency option to promiseAll

--- src/promiseAll.test.ts
+++ src/promiseAll.test.ts
@@ -1,11 +1,56 @@
-import { it } from 'bun:test';
+import { describe, it, expect } from 'bun:test';
 import { equal } from './internal/test-utils';
 import { iter, map, promiseAll, range, tee, toArray } from '.';
-it('promiseAll', async function () {
-  const sleep = (ms: number): Promise<number> => new Promise(resolve => setTimeout(() => resolve(ms), ms));
-  const [it1, it2] = tee(
-    iter(range(10)).map(n => n * 10),
-    2,
-  );
-  equal(await promiseAll(map(it1, sleep)), toArray(it2));
+
+describe('promiseAll', () => {
+  it('should return the results of the promises', async function () {
+    const sleep = (ms: number): Promise<number> => new Promise(resolve => setTimeout(() => resolve(ms), ms));
+    const [it1, it2] = tee(
+      iter(range(10)).map(n => n * 10),
+      2,
+    );
+    equal(await promiseAll(map(it1, sleep)), toArray(it2));
+  });
+
+  it('concurrency 1', async () => {
+    let active = 0;
+    let maxActive = 0;
+    const n = 4;
+    const ms = 100;
+    const sleep = (ms: number): Promise<number> =>
+      new Promise(resolve => {
+        active++;
+        maxActive = Math.max(maxActive, active);
+        setTimeout(() => {
+          active--;
+          resolve(ms);
+        }, ms);
+      });
+    const expected = iter(range(n)).map(() => ms).toArray();
+    const it = iter(range(n)).map(() => sleep(ms));
+    const result = await promiseAll(it, { concurrency: 1 });
+    expect(result).toEqual(expected);
+    expect(maxActive).toBe(1);
+  });
+
+  it('concurrency 2', async () => {
+    let active = 0;
+    let maxActive = 0;
+    const n = 4;
+    const ms = 100;
+    const sleep = (ms: number): Promise<number> =>
+      new Promise(resolve => {
+        active++;
+        maxActive = Math.max(maxActive, active);
+        setTimeout(() => {
+          active--;
+          resolve(ms);
+        }, ms);
+      });
+    const expected = iter(range(n)).map(() => ms).toArray();
+    const it = iter(range(n)).map(() => sleep(ms));
+    const result = await promiseAll(it, { concurrency: 2 });
+    expect(result).toEqual(expected);
+    expect(maxActive).toBe(2);
+  });
 });
